ca132e51610d2abdb6173d66711cf077a4dc8fdb
unknown <b.ahanna@yahoo.com>
2022-03-29 14:16:04 -0400

    coding challenge turning string into an array of words

--- challenges.js
+++ challenges.js
@@ -2,3 +2,11 @@
 
 //1. Create a function that multiples 2 numbers
 let multiply = (num1, num2) => num1 * num2;
+
+//2.Create a function that takes a string and turns it into an array of words
+//my solution
+function stringToArray(string) {
+  return string.split(" ");
+}
+//as an arrow function
+const stringToArray = (string) => string.split(" ");
