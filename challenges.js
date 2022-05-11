689c8dd2708933f92dfb8af12f326734f1cd5007
unknown <b.ahanna@yahoo.com>
2022-05-10 21:36:39 -0400

    array challenge

--- challenges.js
+++ challenges.js
@@ -166,3 +166,10 @@ function countBy(x, n) {
 function multipleOfIndex(array) {
   return array.filter((el, index) => el % index === 0);
 }
+//I'm new to coding and now I want to get the sum of two arrays...actually the sum of all their elements. I'll appreciate for your help.
+
+//P.S. Each array includes only integer numbers. Output is a number too.
+
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a, c) => a + c, 0);
+}
