7bafaed8a29d65564082aa45034687366a2c191a
unknown <b.ahanna@yahoo.com>
2022-05-18 14:34:42 -0400

    code challenges array and string

--- challenges5/15.js
+++ challenges5/15.js
@@ -68,3 +68,15 @@ function filter_list(l) {
   return l.filter((el) => el.toString() !== el);
 }
 
+//Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+function stringClean(s) {
+  return s
+    .split("")
+    .filter((n) => n != parseInt(n))
+    .join("");
+}
+
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin){
+  return parseInt(bin,2);
+}
