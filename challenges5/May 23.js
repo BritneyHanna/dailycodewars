e645ea7487c6fcd15d6c63f1faf2507421f95761
unknown <b.ahanna@yahoo.com>
2022-05-31 09:24:42 -0400

    find the highest scoring word coding challenge

--- challenges5/May 23.js
+++ challenges5/May 23.js
@@ -47,8 +47,10 @@ function sumOfDifferences(arr) {
   return total;
 }
 //Other solution
-const sumOfDifferences = (arr) =>
+const sumOfDifferences2 = (arr) =>
   arr
     .sort((a, b) => b - a)
     .map((a, i) => a - arr[i + 1] || 0)
     .reduce((a, b) => a + b, 0);
+
+    
