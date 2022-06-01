f3d7cc01446753ae4c373cd320c657af6a072005
unknown <b.ahanna@yahoo.com>
2022-06-01 08:44:39 -0400

    test

--- challenges5/May/May31.js
+++ challenges5/May/May31.js
@@ -29,7 +29,3 @@ function billboard(name, price = 30) {
     return a + price;
   }, 0);
 } 
-
-//Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
-
-// Note that the number will always be non-negative (>= 0).
