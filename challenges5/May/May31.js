b4664a84b33ed2f0f4292082f42c9a131547d738
unknown <b.ahanna@yahoo.com>
2022-06-01 08:23:21 -0400

    codewars name on billboard kata

--- challenges5/May/May31.js
+++ challenges5/May/May31.js
@@ -15,3 +15,17 @@ function high(s) {
     .map((s) => [...s].reduce((a, b) => a + b.charCodeAt(0) - 96, 0));
   return s.split(" ")[as.indexOf(Math.max(...as))];
 }
+
+
+// You can print your name on a billboard ad. Find out how much it will cost you. Each letter has a default price of £30, but that can be different if you are given 2 parameters instead of 1.
+
+// You can not use multiplier "*" operator.
+
+// If your name would be Jeong-Ho Aristotelis, ad would cost £600. 20 leters * 30 = 600 (Space counts as a letter).
+
+function billboard(name, price = 30) {
+  name = name.split("");
+  return name.reduce((a, c) => {
+    return a + price;
+  }, 0);
+} 
