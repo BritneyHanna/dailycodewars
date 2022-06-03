0e172cbc29ca052315eb4ddb599890cfb6a4bba4
unknown <b.ahanna@yahoo.com>
2022-06-03 14:19:59 -0400

    insert dashes kata

--- challenges5/May/May31.js
+++ challenges5/May/May31.js
@@ -30,4 +30,19 @@ function billboard(name, price = 30) {
   }, 0);
 } 
 
-//
+//Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes ('-') between each two odd digits in num. For example: if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
+//Note that the number will always be non-negative (>= 0).
+//****** Need to review  */
+
+
+function insertDash(num) {
+  return num
+    .toString()
+    .split("")
+    .reduce((acc, c, i, arr) => {
+      if (i > 0 && Number(arr[i]) % 2 !== 0 && Number(arr[i - 1]) % 2 !== 0)
+        acc = acc + "-";
+
+      return acc + c;
+    }, "");
+}
