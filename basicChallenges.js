49e053e5d2e8551fe4d46cf47c17da88af19fca7
unknown <b.ahanna@yahoo.com>
2022-06-06 14:23:36 -0400

    get divisor count code challenge

--- basicChallenges.js
+++ basicChallenges.js
@@ -223,7 +223,7 @@ let stringToNumberc = function (str) {
 
 //Count the number of divisors of a positive integer n.
 
-// Random tests go up to n = 500000.
+// Random tests go up to n = 500000. Note 0 is not a divisor 
 function getDivisorsCnt(n) {
   let count = 0;
   for (let i = 1; i <= n; i++) {
