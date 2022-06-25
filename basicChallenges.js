a43f4051805e2064683ecbd1cb6af2c58911b995
unknown <b.ahanna@yahoo.com>
2022-06-25 12:20:19 -0400

    code challenge even or odd

--- basicChallenges.js
+++ basicChallenges.js
@@ -287,3 +287,6 @@ const reverseSeq = (n) => {
   }
   return arr;
 };
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+const even_or_odd = (number) => (number % 2 === 0 ? "Even" : "Odd");
