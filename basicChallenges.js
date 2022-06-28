45efbcf46525a477c6de57d08cf7e2a8aebeed31
unknown <b.ahanna@yahoo.com>
2022-06-27 21:37:47 -0400

    basic function coding challenge

--- basicChallenges.js
+++ basicChallenges.js
@@ -303,3 +303,9 @@ function capitalizeWord(word) {
   const newword = word[0].toUpperCase().concat(word.slice(1));
   return newword;
 }
+//Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+//The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+function setAlarm(employed, vacation) {
+  return employed && !vacation;
+}
