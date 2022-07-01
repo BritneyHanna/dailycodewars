d266d8967b421930b595c609c40bb414c1003706
unknown <b.ahanna@yahoo.com>
2022-06-30 21:53:57 -0400

    fundamentals code challenge dates

--- basicChallenges.js
+++ basicChallenges.js
@@ -315,3 +315,9 @@ const reverseList = (list) => list.reverse();
 function opposite(number) {
   return -1 * number;
 }
+//Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+//Make sure that your function does not return a false positive by only checking the day.
+function isToday(date) {
+  return new Date().toDateString() === date.toDateString();
+}
