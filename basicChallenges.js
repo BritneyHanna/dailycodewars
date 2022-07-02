342c65f6a822c6413dcb82ffefc3286b5e65d0cc
unknown <b.ahanna@yahoo.com>
2022-07-01 21:58:43 -0400

    fundamentals code challenge

--- basicChallenges.js
+++ basicChallenges.js
@@ -321,3 +321,10 @@ function opposite(number) {
 function isToday(date) {
   return new Date().toDateString() === date.toDateString();
 }
+//I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+// Can you help me fix the function?
+function addFive(num) {
+  let total = num + 5;
+  return total;
+}
