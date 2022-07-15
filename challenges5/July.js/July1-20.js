7d0500aac5b3b1fcdfebcd92732a80cdb74e39ac
unknown <b.ahanna@yahoo.com>
2022-07-15 12:05:09 -0400

    Object oriented piracy solution

--- challenges5/July.js/July1-20.js
+++ challenges5/July.js/July1-20.js
@@ -165,3 +165,28 @@ Math.roundTo = function (number, precision) {
 
 //alternative 
 Math.roundToB = (number, precision) => Number(number.toFixed(precision));
+//**************************************************************** */
+// Taking into account that an average crew member on board adds 1.5 units to the draft, a ship that has a draft of more than 20 without crew is considered worthy to loot. Any ship weighing that much must have a lot of booty!
+
+// Add the method
+
+// isWorthIt
+// to decide if the ship is worthy to loot. For example:
+
+function Ship(draft, crew) {
+  this.draft = draft;
+  this.crew = crew;
+
+  this.isWorthIt = function () {
+    return this.draft - this.crew * 1.5 >= 20;
+  };
+}
+// I prefer this solution
+function Ship(draft, crew) {
+  this.draft = draft;
+  this.crew = crew;
+}
+
+Ship.prototype.isWorthItB = function () {
+  return this.draft - this.crew * 1.5 > 20;
+};
