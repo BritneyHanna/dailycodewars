5073230fac31e6d11066f54b850b87167e216615
unknown <b.ahanna@yahoo.com>
2022-07-16 12:10:21 -0400

    fundamentals challenge OOP

--- challenges5/July.js/July1-20.js
+++ challenges5/July.js/July1-20.js
@@ -190,3 +190,18 @@ function Ship(draft, crew) {
 Ship.prototype.isWorthItB = function () {
   return this.draft - this.crew * 1.5 > 20;
 };
+
+//Create a class Ghost
+
+// Ghost objects are instantiated without any arguments.
+
+// Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+
+// ghost = new Ghost();
+// ghost.color //=> "white" or "yellow" or "purple" or "red"
+
+let Ghost = function () {
+  this.color = ["white", "yellow", "purple", "red"][
+    Math.floor(Math.random() * 4)
+  ];
+};
