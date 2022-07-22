30c7040335a05fb85ebe1c24b97149f903a59458
unknown <b.ahanna@yahoo.com>
2022-07-22 12:33:13 -0400

    fundamentals challenge variables

--- challenges5/July.js/July1-22.js
+++ challenges5/July.js/July1-22.js
@@ -271,3 +271,24 @@ function predictAge(...age) {
 
   return Math.floor(Math.sqrt(sum) / 2);
 }
+//VVhat ?!?
+
+// None of zese codevarriors seemz to remember hiz ovvn name !
+
+// Kould you help ?
+
+// var albert = new Warrior("Al")
+// var boris  = new Warrior("Boris")
+
+// albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+function Warrior(n) {
+  let name = n;
+  this.name = function (n) {
+    if (n) name = n;
+    return name;
+  };
+}
+
+Warrior.prototype.toString = function () {
+  return "Hi! my name's " + this.name();
+};
