e5d44c7de8774d2082de6ac660457b2cac3b13ae
unknown <b.ahanna@yahoo.com>
2022-07-23 12:40:04 -0400

    fundamentals challenge Objects

---
+++ challenges5/July.js/July23.js
@@ -0,0 +1,13 @@
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+function Dog(breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function () {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
