f3177c598d2e42fca6c3fb21f9c39be72bbae1a1
unknown <b.ahanna@yahoo.com>
2022-07-24 17:26:55 -0400

    fundamentals challenge

--- challenges5/July.js/July23.js
+++ challenges5/July.js/July23.js
@@ -11,3 +11,48 @@ Dog.prototype.bark = function () {
 };
 
 var scoobydoo = new Dog("Great Dane");
+//**************************************************************************** */
+// Here is a piece of code:
+
+// function getStatus(isBusy) {
+//   var msg = (isBusy ? "busy" : "available");
+//   return
+//   {
+//     status: msg
+//   }
+// }
+// Expected Behaviour
+// Function should return a dictionary/Object/Hash with "status" as a key, whose value can : "busy" or "available" depending on the truth value of the argument is_busy.
+
+// But as you will see after clicking RUN or ATTEMPT this code has several bugs, please fix them.
+
+
+const getStatus = (isBusy) => {
+  let msg = isBusy ? "busy" : "available";
+  return { status: msg };
+};
+
+//More concise syntax I found on codewars 
+const getStatusB = (isBusy) => ({ status: isBusy ? "busy" : "available" });
+
+//**************************************************** */
+//Come here to practice the Arrow style functions Not much else to say good luck!
+// Details
+// You will be given an array of numbers which can be used using the String.fromCharCode() (JS), Tools.FromCharCode() (C#) method to convert the number to a character. It is recommended to map over the array of numbers and convert each number to the corresponding ascii character.
+
+// Examples
+// These are example of how to convert a number to an ascii Character:
+// Javascript => String.fromCharCode(97) // a
+// C# => Tools.FromCharCode(97) // a
+let ArrowFunc = (arr) => arr.map((el) => String.fromCharCode(el)).join("");
+
+
+//The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+function numberToPower(number, power) {
+  let num = 1;
+  for (let i = 1; i <= power; i++) {
+    num = num * number;
+  }
+  return num;
+}
+
