bdd060a4f5786d678a8c99401be49da4ecd4ca80
unknown <b.ahanna@yahoo.com>
2022-07-29 17:45:59 -0400

    fundamentals challenge url encode function

--- challenges5/July.js/July23-31.js
+++ challenges5/July.js/July23-31.js
@@ -127,3 +127,14 @@ Array.prototype.filter = function (func) {
   }
   return answer;
 };
+//***************************** */
+
+// Generate user links
+// Your task is to create userlinks for the url, you will be given a username and must return a valid link.
+
+// Example
+// generate_link('matt c')
+// http://www.codewars.com/users/matt%20c
+
+let generateLink = (user) =>
+  `http://www.codewars.com/users/${encodeURIComponent(user)}`;
