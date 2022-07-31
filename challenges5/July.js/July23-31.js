540509ec8ee4e46517a61f405893893280ef8fa4
unknown <b.ahanna@yahoo.com>
2022-07-31 16:22:09 -0400

    fundamentals challenge

--- challenges5/July.js/July23-31.js
+++ challenges5/July.js/July23-31.js
@@ -151,3 +151,10 @@ let circular = {
   value: "Hello World",
 };
 circular.self = circular;
+//The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+
+const cockroachSpeed = (s) => Math.floor(s * 27.7778);
