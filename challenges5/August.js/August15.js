998246f916f74c8f82a63d08d19957cfe8e56cd1
unknown <b.ahanna@yahoo.com>
2022-08-15 11:22:05 -0400

    code challange find the anonymous function

---
+++ challenges5/August.js/August15.js
@@ -0,0 +1,10 @@
+// Find the anonymous function in the given array and use the function to filter the array
+// Input
+// Your input. First Parameter will be an array with an anonymous function somewhere in the lot, The second Parameter will be an array which you will filter using the anonymous function you find.
+// Output
+// Your output. Output a filtered version of the second parameter using the function found in the first parameter.
+
+const FindFunction = function (func, arr) {
+  const anonymous = func.find((el) => typeof el == "function");
+  return arr.filter(anonymous);
+};
