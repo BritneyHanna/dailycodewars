9bfe5bd297e1c913e7c8e171a6a27844ee76bbc9
unknown <b.ahanna@yahoo.com>
2022-09-07 12:24:19 -0400

    simple hash table question

--- challenges5/September/September6.js
+++ challenges5/September/September6.js
@@ -1,5 +1,5 @@
 //debug function with a switch case
-//I just added return statements I could also have used a break 
+//I just added return statements I could also have used a break
 
 function getPlanetName(id) {
   let name;
@@ -27,15 +27,34 @@ function getPlanetName(id) {
 
 // Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 const maxSubArray = (arr) => {
-    let maxSum = arr[0];
-    let currentSum = arr[0];
- 
-    for (let i = 1; i < arr.length; i++){
-      //compare the first num the array to the value of the first+second numbers assign current sum to larger value
-        currentSum = Math.max(currentSum + arr[i], arr[i]);
-        //compare the current value to the max value reassign to the larger value 
-      maxSum = Math.max(currentSum, maxSum);
+  let maxSum = arr[0];
+  let currentSum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    //compare the first num the array to the value of the first+second numbers assign current sum to larger value
+    currentSum = Math.max(currentSum + arr[i], arr[i]);
+    //compare the current value to the max value reassign to the larger value
+    maxSum = Math.max(currentSum, maxSum);
+  }
+  // solution asked for the total not the sub array itself
+  return maxSum;
+};
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+const twoSum = (nums, targetNum) => {
+  let map = new Map();
+  result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      result.push(i, map.get(nums[i]));
+    } else {
+      map.set(targetNum - nums[i], i);
     }
-    // solution asked for the total not the sub array itself 
-    return maxSum
-}
+  }
+  return result;
+};
---
+++ challenges5/September/September7 HashMap .js
@@ -0,0 +1,47 @@
+//given an array find the first recurring number
+
+const lookup = (arr) => {
+  let seen = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (seen.includes(arr[i])) {
+      return arr[i];
+    } else {
+      seen.push(arr[i]);
+      console.log(seen);
+    }
+  }
+  return undefined;
+};
+
+const findDuplicate = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    //ste the value of j to one right of the value of i hence I used i+1
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return arr[i];
+      }
+    }
+  }
+  return undefined;
+};
+
+//I think this is quadratic time (need to verify) since the .includes array method search is o(n) and the for loop is also o(n). Same as if I used a nested for loop
+
+//Better solution O(n) 
+
+const findDuplicates = (arr) => {
+  let map = {};
+  for (let i = 0; i < arr.length; i++){
+    //this is saying if the map obj has a key with the same value as the value at the index of the array 
+    if (map[arr[i]] !==undefined) {
+      return arr[i]
+    } else {
+      //if not make a property which goes by that name in this case I assigned the property a value of true 
+      map[arr[i] ]=true
+    }
+  }
+  return undefined
+}
+
+// the time complexity is linear because search in maps is 0(1) and the for loop is O(N)
+
