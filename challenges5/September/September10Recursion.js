6e1e6ff8896f54df41935d235eb5753eb177a44c
unknown <b.ahanna@yahoo.com>
2022-09-09 12:21:23 -0400

    recursion challenge

---
+++ challenges5/September/September10Recursion.js
@@ -0,0 +1,21 @@
+// Find the factorial of a number Recursive method 
+const findFactorial = (num) => {
+    if (num <= 2) {
+        return 2 
+    }
+  
+    return num*findFactorial(num-1)
+}
+
+console.log(findFactorial(5));
+
+
+Iterative 
+function findFactorialIterative(number) {
+  let answer = 1;
+// we start at 2 because 1*1 is 1 doesnt make a diff
+  for (let i = 2; i <= number; i++) {
+    answer = answer * i;
+  }
+  return answer;
+}
