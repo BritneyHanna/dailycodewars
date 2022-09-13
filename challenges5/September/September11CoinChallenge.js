6ac19cfed59b2bb242a3128c45ec42d9a9c69a9b
unknown <b.ahanna@yahoo.com>
2022-09-13 13:21:46 -0400

    recursive solution coin challenge

--- challenges5/September/September11CoinChallenge.js
+++ challenges5/September/September11CoinChallenge.js
@@ -12,3 +12,20 @@
 
 // Output#
 // The number of ways to get the amount using the coins available
+//recursive 
+function returnWays(coins, numOfCoins, amount) {
+  if (amount === 0) return 1; // only one way to return zero amount
+
+  if (amount < 0) return 0; // No solution exists for negative amount
+
+  if (numOfCoins < 0 && amount > 0) return 0; // If no coins left
+
+  return (
+    returnWays(coins, numOfCoins, amount - coins[numOfCoins]) +
+    returnWays(coins, numOfCoins - 1, amount)
+  );
+}
+
+var coins = [1, 2, 3];
+var amount = 4;
+console.log(returnWays(coins, coins.length - 1, amount));
