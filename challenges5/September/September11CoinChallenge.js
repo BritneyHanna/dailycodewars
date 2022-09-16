e6caa8ebf3c8c02fe47b74a67bab59d9186e7989
unknown <b.ahanna@yahoo.com>
2022-09-16 13:07:40 -0400

    added cheatsheet link for DS

--- challenges5/September/September11CoinChallenge.js
+++ challenges5/September/September11CoinChallenge.js
@@ -57,3 +57,38 @@ var coins = [1, 2, 3];
 var amount = 4;
 console.log(returnWays(amount, coins));
 
+function main(listsize, distances, start, end) {
+  //I want a list of the numbers that are between start and end
+  //if the distances are an array I can just filter for the ones >start<end or I can do a for loop since I know the length of the list
+  //r=the output says to print the space separated integars so maybe I can just console log?
+  //Ill save the ints in an array
+  let result = [];
+  for (let i = 1; i <= listsize; i++) {
+    if (i > start && i < end) {
+      result.push(i);
+    }
+  }
+  if (result.length > 0) {
+    return result;
+  } else {
+    return -1;
+  }
+}
+
+
+function main(orderPlaced_size, N, K) {
+  // so we check the list every 3 elements at a time and we return the first element that is less than 0,
+  //Ok so I thought this wrong I would check every three elements and then the first of those elements that is less than zero is what I would return  
+  //return the array
+  let result = [];
+  for (let i = 0; i < orderPlaced_size - K; i++) {
+    for (let j = i; j < i + K; j++) {
+      if (orderPlaced_size[j] < 0) {
+        result.push(orderPlaced_size[j])
+      }
+
+    }
+    return result
+  }
+  return -1
+}
