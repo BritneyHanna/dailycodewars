1ff19b957dbb37c37ba1630babf967a04799038b
unknown <b.ahanna@yahoo.com>
2022-09-26 22:12:22 -0400

    corrected steps in 2sum challenge

--- challenges5/September/September21CoderbyteChallenges.js
+++ challenges5/September/September21CoderbyteChallenges.js
@@ -4,7 +4,7 @@
 //loop through the list and for each element find the diff between target and num[i]
 //check if that diff exists as a key in the map
 //if it does then return the map[diff ] which gives the index of tht value as well as the index of the current element i
-//if it does then add the value of the number at the current index to the map 
+//if it doesnt then add the value of the number at the current index to the map 
 
 const twoSum = function (nums, target) {
   const map = {};
